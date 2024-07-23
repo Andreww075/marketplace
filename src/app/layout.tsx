731e8b8c74d216e38db5deb11333343488b280a2
Nicolás Gómez <Nvandres079@gmail.com>
2024-07-23 10:24:35 -0500

    Header Component and authentication added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,14 +12,18 @@ export const metadata: Metadata = {
   description: "Marketplace created with NextJs, Typescript, Tailwind.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession(authOptions);
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header session={session} />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { signIn, signOut } from "next-auth/react";
+import { Session } from "next-auth";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Image from "next/image";
+
+const Header = ({ session }: { session: Session | null }) => {
+  const router = useRouter();
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  return (
+    <header className="border-b p-4 flex items-center justify-between h-16">
+      {/* Logo */}
+
+      <Link className="text-blue-600 font-bold text-2xl" href="/">
+        Marketplace
+      </Link>
+
+      {/* nav */}
+
+      <nav className="flex items-center gap-4 *:rounded">
+        <Link
+          href="/"
+          className="border border-blue-600 text-blue-600 inline-flex gap-1 items-center py-1 px-4 mr-4"
+        >
+          <FontAwesomeIcon icon={faPlus} className="h-4" />
+          <span>Post an ad</span>
+        </Link>
+
+        <span className="border-r"></span>
+
+        {!session?.user && (
+          <>
+            <button
+              onClick={() => signIn("google")}
+              className="border-0 text-gray-600"
+            >
+              Sign up
+            </button>
+            <button
+              onClick={() => signIn("google")}
+              className="bg-blue-600 text-white border-0 px-6 py-1"
+            >
+              Login
+            </button>
+          </>
+        )}
+
+        {session?.user && (
+          <>
+            <div className="relative flex items-center">
+              <Link href='/account'>
+                <Image
+                  src={session.user.image as string}
+                  alt={"avatar"}
+                  width={36}
+                  height={36}
+                  className={
+                    "rounded-md relative " + (showDropdown ? "z-50" : "")
+                  }
+                />
+              </Link>
+
+              <button className="p-2 block w-full" onClick={() => signOut()}>
+                Logout
+              </button>
+            </div>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
