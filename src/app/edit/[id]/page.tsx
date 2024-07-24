1dfb4ea95f555654943dbfdf1d396901a2e4b62b
Nicolás Gómez <Nvandres079@gmail.com>
2024-07-24 17:43:00 -0500

    Added edit ad functionality

---
+++ src/app/edit/[id]/page.tsx
@@ -0,0 +1,43 @@
+"use server";
+
+import React from "react";
+import AdForm from "@/components/ad-form";
+import { authOptions } from "@/libs/authOptions";
+import { connect } from "@/libs/helpers";
+import { AdModel } from "@/models/Ad";
+import { getServerSession } from "next-auth";
+
+type Props = {
+  params: {
+    id: string;
+  };
+  searchParams: { [key: string]: string };
+};
+
+const EditPage = async (props: Props) => {
+  const id = props.params.id;
+  await connect();
+  const session = await getServerSession(authOptions);
+
+  const adDoc = await AdModel.findById(id);
+  if (!adDoc) {
+    return "404 not found";
+  }
+  if (session?.user?.email !== adDoc?.userEmail) {
+    return "not yours";
+  }
+
+  return (
+    <AdForm
+      id={adDoc._id}
+      defaultTexts={adDoc}
+      defaultFiles={adDoc.files}
+      defaultLocation={{
+        lng: adDoc.location.coordinates[0],
+        lat: adDoc.location.coordinates[1],
+      }}
+    />
+  );
+};
+
+export default EditPage;
