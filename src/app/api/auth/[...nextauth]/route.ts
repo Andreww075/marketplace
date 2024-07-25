6b8a455a452f51b08bfeeb8ab20a9c6e4f4dc07f
Nicolás Gómez <Nvandres079@gmail.com>
2024-07-25 15:05:11 -0500

    .

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
-import NextAuth from "next-auth";
+import NextAuth from "next-auth/next";
 import { authOptions } from "@/utils/authOptions";
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST, authOptions };
+export { handler as GET, handler as POST };
